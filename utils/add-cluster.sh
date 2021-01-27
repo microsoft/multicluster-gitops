f1020f472b1e26e024c1650a13538ebbad5129fc
Eugene <eugene.fedor@gmail.com>
2021-01-27 14:30:46 -0800

    bootstrap

---
+++ utils/add-cluster.sh
@@ -0,0 +1,31 @@
+ #!/bin/bash
+
+# Usage:
+# 
+# export GITHUB_TOKEN=<your-token>
+# export GITHUB_USER=<your-username>
+# export GITHUB_REPO=<repository-name>
+#
+# add-cluster.sh CLUSTER_NAME
+
+CLUSTER_NAME=$1
+
+mkdir -p  ./clusters/$CLUSTER_NAME
+cp ./utils/templates/cluster-infra-flux-kustomization.yaml ./clusters/$CLUSTER_NAME/infra.yaml
+sed -i 's/{CLUSTER_NAME}/'$CLUSTER_NAME'/g' ./clusters/$CLUSTER_NAME/infra.yaml
+
+mkdir -p  ./infra/$CLUSTER_NAME
+cp ./utils/templates/cluster-infra-k8s-kustomization.yaml ./infra/$CLUSTER_NAME/kustomization.yaml
+
+
+flux bootstrap github \
+    --owner=${GITHUB_USER} \
+    --repository=${GITHUB_REPO} \
+    --branch=main \
+    --path=clusters/base
+
+kubectl apply -f clusters/$CLUSTER_NAME/infra.yaml
+
+
+
+
