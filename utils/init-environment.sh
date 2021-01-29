9e79eec03375b0ddcd10ba9c60e0fc37e812e964
Eugene <eugene.fedor@gmail.com>
2021-01-28 16:09:39 -0800

    init environment

--- utils/init-environment.sh
+++ utils/init-environment.sh
@@ -2,18 +2,16 @@
 
 # Usage:
 # 
-# init-environment.sh ENV_NAME REPO_URL
+# init-environment.sh ENV_NAME
 
 
 ENV_NAME=$1
-REPO_URL=$2
 
 echo $ENV_NAME > ENV_NAME
 
 rm -r -f ./clusters/*
 mkdir ./clusters/base
 cp ./utils/templates/clusters/base/* ./clusters/base/
-./utils/update-name.sh ./clusters/base/ 'REPO_URL' $REPO_URL
 ./utils/update-name.sh ./clusters/base/ 'ENV_NAME' $ENV_NAME
 
 rm -r -f ./infra/*
