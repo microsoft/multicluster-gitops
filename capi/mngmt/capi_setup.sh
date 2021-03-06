d80d73574a3f3f81808049454f959fe94be07933
Eugene <eugene.fedor@gmail.com>
2021-03-05 16:35:49 -0800

    cleaning

---
+++ capi/mngmt/capi_setup.sh
@@ -0,0 +1,16 @@
+export AZURE_SUBSCRIPTION_ID=
+export AZURE_TENANT_ID=
+export AZURE_CLIENT_ID=
+export AZURE_CLIENT_SECRET=
+
+export AZURE_ENVIRONMENT="AzurePublicCloud"
+
+export AZURE_SUBSCRIPTION_ID_B64="$(echo -n "$AZURE_SUBSCRIPTION_ID" | base64 | tr -d '\n')"
+export AZURE_TENANT_ID_B64="$(echo -n "$AZURE_TENANT_ID" | base64 | tr -d '\n')"
+export AZURE_CLIENT_ID_B64="$(echo -n "$AZURE_CLIENT_ID" | base64 | tr -d '\n')"
+export AZURE_CLIENT_SECRET_B64="$(echo -n "$AZURE_CLIENT_SECRET" | base64 | tr -d '\n')"
+
+export EXP_MACHINE_POOL=true
+export EXP_AKS=true
+
+clusterctl init --infrastructure azure
