e870e201e8f6e41444429f1b810617e192d658a0
Eugene <eugene.fedor@gmail.com>
2021-07-27 14:26:01 -0700

    scripts

--- capi/setup/aws/capa_setup.sh
+++ capi/setup/aws/capa_setup.sh
@@ -1,17 +1,20 @@
-# Install cluster ctl https://cluster-api.sigs.k8s.io/user/quick-start.html#install-clusterctl
-export AZURE_SUBSCRIPTION_ID=
-export AZURE_TENANT_ID=
-export AZURE_CLIENT_ID=
-export AZURE_CLIENT_SECRET=
+# Install cluster ctl 
+# https://cluster-api.sigs.k8s.io/user/quick-start.html#install-clusterctl
 
-export AZURE_ENVIRONMENT="AzurePublicCloud"
+# Download and install clusterawsadm 
+# https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases
 
-export AZURE_SUBSCRIPTION_ID_B64="$(echo -n "$AZURE_SUBSCRIPTION_ID" | base64 | tr -d '\n')"
-export AZURE_TENANT_ID_B64="$(echo -n "$AZURE_TENANT_ID" | base64 | tr -d '\n')"
-export AZURE_CLIENT_ID_B64="$(echo -n "$AZURE_CLIENT_ID" | base64 | tr -d '\n')"
-export AZURE_CLIENT_SECRET_B64="$(echo -n "$AZURE_CLIENT_SECRET" | base64 | tr -d '\n')"
+export AWS_REGION=
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export EXP_EKS=true
+export EXP_EKS_IAM=true
+export EXP_EKS_ADD_ROLES=true
 
-export EXP_MACHINE_POOL=true
-export EXP_AKS=true
+clusterawsadm bootstrap iam create-cloudformation-stack --config bootstrap-config.yaml
 
-clusterctl init --infrastructure azure
+export AWS_B64ENCODED_CREDENTIALS=$(clusterawsadm bootstrap credentials encode-as-profile)
+
+clusterctl init --infrastructure aws --control-plane aws-eks --bootstrap aws-eks
+
+kubectl apply -f role-binding.yaml
